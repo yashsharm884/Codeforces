c21921a25f83810dce3cba75ce4e1431ed910729
Yash <yashsharm884@gmail.com>
2022-09-11 21:43:58 +0530

    ICPC Assiut University Community

---
+++ Basic_data_type.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+#define ll long long 
+using namespace  std; 
+
+void solve() 
+{
+    int a; cin >> a; 
+    ll b; cin >> b; 
+    char ch ; cin >> ch; float c ; cin >> c; 
+    double d ; cin >> d; 
+
+    cout << a << endl << b << endl << ch << endl << c << endl << d ;    
+}
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+    #endif 
+
+    solve(); 
+}
