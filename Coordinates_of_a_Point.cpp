185588595834fb23f0d7ba8c891670830506c5b3
Yash <yashsharm884@gmail.com>
2022-09-12 11:16:39 +0530

    ICPC Assiut University Community

---
+++ Coordinates_of_a_Point.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h> 
+#define ll long long 
+using namespace  std; 
+
+void solve() 
+{
+    double a, b ; cin >> a >> b ; 
+
+    if(a > 0 && b < 0) 
+    {
+        cout << "Q4" ; 
+    }
+    else if(a > 0 && b > 0)
+    {
+        cout << "Q1" ; 
+    }
+    else if(a < 0 && b > 0) 
+    {
+        cout << "Q2" ; 
+    }
+    else if(a < 0 && b < 0)
+    {
+        cout << "Q3" ; 
+    }
+    else if(a == 0 && b == 0)
+    {
+        cout << "Origem" ;
+    }
+    else if (a == 0 && b != 0) 
+    {
+        cout << "Eixo Y" ; 
+    }
+    else if(a != 0 && b == 0) 
+    {
+        cout << "Eixo X" ; 
+    }
+}
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+    #endif 
+
+    solve(); 
+}
