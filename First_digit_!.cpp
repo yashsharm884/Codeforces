227dd0e45d89c7c723895c551f57bca82a42b906
Yash <yashsharm884@gmail.com>
2022-09-12 11:16:48 +0530

    ICPC Assiut University Community

---
+++ First_digit_!.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h> 
+#define ll long long 
+using namespace  std; 
+
+void solve() 
+{
+   int n; cin >> n;  
+
+    while (n >= 10) 
+    {
+        n = n / 10; 
+    }
+
+    if(n % 2 == 0) 
+    {
+        cout << "EVEN" ; 
+    }
+    else 
+    {
+        cout << "ODD" ; 
+    }
+   
+}
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+    #endif 
+
+    solve(); 
+}
