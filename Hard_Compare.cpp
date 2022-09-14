2f880258514dcee4337acc85dc42d451a77b8356
yashcloudAcc <yash.sharma@cloudanalogy.com>
2022-09-14 17:10:51 +0530

    ICPC Assiut University Community

---
+++ Hard_Compare.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+#define ll long long 
+#define ld long double 
+using namespace  std; 
+
+void solve() 
+{
+   ld a,b,c,d;  cin >> a >> b >> c >> d; 
+   ld e = log(a) * b ;
+   ld f = log(c) * d; 
+
+   if(e > f) 
+   {
+        cout << "YES" ; 
+   }
+   else 
+   {
+        cout << "NO" ; 
+   }
+}
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+    #endif 
+
+    solve(); 
+}
